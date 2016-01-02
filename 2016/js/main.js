4283a73e99374d2f452146fcbf24f7a6d3500738
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-01-03 00:08:52 +0900

    implement eyes

---
+++ 2016/js/main.js
@@ -0,0 +1,41 @@
+$(function () {
+  'use strict';
+
+  function closeEyes() {
+    $eyeOpen.hide();
+    $eyeClose.show();
+  }
+
+  function openEyes() {
+    $eyeOpen.show();
+    $eyeClose.hide();
+  }
+
+  var $saru = $('.saru');
+
+  var $eyeOpen = $saru.find('.saru--animal-character_eyes_open');
+  var $eyeClose = $saru.find('.saru--animal-character_eyes_close');
+
+  var eventList = {
+    OPENEYES: 'OPENEYES',
+    CLOSEEYES: 'CLOSEEYES',
+  };
+
+  $saru.on(eventList.OPENEYES, function () {
+    var delay = 500 + 5000 * Math.random();
+    openEyes();
+    setTimeout(function () {
+      $saru.trigger(eventList.CLOSEEYES);
+    }, delay);
+  });
+
+  $saru.on(eventList.CLOSEEYES, function () {
+    var delay = 50 + 100 * Math.random();
+    closeEyes();
+    setTimeout(function () {
+      $saru.trigger(eventList.OPENEYES);
+    }, delay);
+  });
+
+  $saru.trigger(eventList.OPENEYES);
+});
