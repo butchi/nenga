1b10b0cc89f32510a60c5a9e327b193127c06b24
IWABUCHI Yu(u)ki <butc.hiyu+com-github@gmail.com>
2018-12-28 00:55:12 +0900

    adapt vuetify

--- src/2019/src/views/Home.vue
+++ src/2019/src/views/Home.vue
@@ -1,28 +1,13 @@
 <template lang="pug">
-  .home
-    img(src="../assets/logo.png" alt="Vue logo" @click="addCount")
-    p {{ count }}
-    HelloWorld(msg="Welcome to Your Vue.js App")
+  hello-world
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "../components/HelloWorld";
 
 export default {
-  name: "home",
   components: {
-    HelloWorld
-  },
-  computed: {
-    ...mapState(["count"])
-  },
-  methods: {
-    ...mapActions(["increment"]),
-    addCount() {
-      this.increment();
-    }
+    "hello-world": HelloWorld
   }
 };
 </script>
