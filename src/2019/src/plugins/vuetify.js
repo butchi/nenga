2d3179ffbc18a87ed0b1da722cd7ef45dcc6fec3
IWABUCHI Yu(u)ki <butc.hiyu+com-github@gmail.com>
2018-12-28 18:31:14 +0900

    lint

--- src/2019/src/plugins/vuetify.js
+++ src/2019/src/plugins/vuetify.js
@@ -3,7 +3,7 @@ import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
 
 // Helpers
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 Vue.use(Vuetify, {
   iconfont: "md",
