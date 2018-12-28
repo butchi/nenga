9e0c2435b3d8c7ce7fc0785ec7d22aafac62fc83
IWABUCHI Yu(u)ki <butc.hiyu+com-github@gmail.com>
2018-12-28 22:15:04 +0900

    implement achievement (under construction)

--- src/2019/src/views/achievement.vue
+++ src/2019/src/views/achievement.vue
@@ -2,17 +2,31 @@
 v-container(fluid, fill-height)
   v-layout(justify-center, align-center)
     v-flex(text-xs-center)
-      | {{ json }}
+      v-card(v-for="ioshishi, index in ioshishiArr", :key="ioshishi.index")
+        //- v-img(:src="imageSrc(index)")
+        v-card-title(primary-title)
+          div
+            h3.title
+              | {{ ioshishi.name }}
+            .rarity
+              | {{ ioshishi.rarity_str }}
+            .description
+              | {{ ioshishi.description }}
 </template>
 
 <script>
-import json from "../data/ioshishi-slot.json";
+import { sheet } from "../data/ioshishi-slot.json";
 
 export default {
   data() {
     return {
-      json
+      ioshishiArr: Object.values(sheet)
     };
+  },
+  methods: {
+    imageSrc(index) {
+      return `img/${this.ioshishiArr[index].friendly_id}.png`;
+    }
   }
 };
 </script>
