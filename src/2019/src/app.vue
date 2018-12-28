813a6cb9f9fdc19585553fccdda875a88c1d12cb
IWABUCHI Yu(u)ki <butc.hiyu+com-github@gmail.com>
2018-12-28 15:48:53 +0900

    rename to lower case

---
+++ src/2019/src/app.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+v-app
+  v-content
+    #nav
+      router-link(to="/")
+        | Home
+      router-link(to="/about")
+        | About
+      router-link(to="/sample")
+        | Sample
+    router-view
+</template>
