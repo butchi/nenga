a44c3f7ecd9760115b59397a7fa6e999155bc389
IWABUCHI Yu(u)ki <butc.hiyu+com-github@gmail.com>
2018-12-28 17:25:04 +0900

    implement layout

--- src/2019/src/app.vue
+++ src/2019/src/app.vue
@@ -1,12 +1,41 @@
 <template lang="pug">
 v-app
+  v-navigation-drawer(v-model='drawer', fixed, app)
+    v-list(dense='')
+      v-list-tile(@click="")
+        v-list-tile-action
+          v-icon home
+        v-list-tile-content
+          v-list-tile-title
+            | ホーム
+      v-list-tile(@click="")
+        v-list-tile-action
+          v-icon contact_mail
+        v-list-tile-content
+          v-list-tile-title
+            | 獲得一覧
+  v-toolbar(color='primary', dark, fixed, app)
+    v-toolbar-side-icon(@click.stop="drawer = !drawer")
+    v-toolbar-title
+      | イ○シシスロット
   v-content
-    #nav
-      router-link(to="/")
-        | Home
-      router-link(to="/about")
-        | About
-      router-link(to="/sample")
-        | Sample
-    router-view
+    v-container(fluid, fill-height)
+      v-layout(justify-center, align-center)
+        v-flex(text-xs-center)
+          | スロット
+  v-footer(color='primary', app='')
+    span.white--text © IWABUCHI Yu(u)ki 2019
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: null
+    };
+  },
+  props: {
+    source: String
+  }
+};
+</script>
--- src/2019/src/plugins/vuetify.js
+++ src/2019/src/plugins/vuetify.js
@@ -2,6 +2,14 @@ import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
 
+// Helpers
+import colors from 'vuetify/es5/util/colors'
+
 Vue.use(Vuetify, {
-  iconfont: "md"
+  iconfont: "md",
+  theme: {
+    primary: colors.deepOrange.darken4, // #BF360C
+    secondary: colors.deepOrange.lighten4, // #FFCCBC
+    accent: colors.orange.base // #FF9800
+  }
 });
