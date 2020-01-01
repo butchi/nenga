432026620329f6e3492855d48978f6673871a349
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2020-01-02 00:08:18 +0900

    add ga

--- src/2020/nuxt.config.js
+++ src/2020/nuxt.config.js
@@ -45,7 +45,10 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-2779957-9',
+    }],
   ],
   /*
    ** Nuxt.js modules
