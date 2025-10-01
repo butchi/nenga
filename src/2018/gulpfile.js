a98f24273bf4b7fce75c517800ca963af6a85d10
butchi <butchiyu+com-github@gmail.com>
2025-10-01 09:19:18 +0900

    lint

--- src/2018/gulpfile.js
+++ src/2018/gulpfile.js
@@ -1,155 +1,165 @@
-'use strict';
+"use strict"
 
 // import
-import gulp from 'gulp';
-import source from 'vinyl-source-stream';
-import sass from 'gulp-sass';
-import sassGlob from 'gulp-sass-glob';
-import pleeease from 'gulp-pleeease';
-import browserify from 'browserify';
-import babelify from 'babelify';
-import pug from 'gulp-pug';
-import rename from 'gulp-rename';
-import uglify from 'gulp-uglify';
-import decodecode from 'gulp-decodecode';
-import browserSync from 'browser-sync';
-import readConfig from 'read-config';
-import watch from 'gulp-watch';
-import RevLogger from 'rev-logger';
-
+import gulp from "gulp"
+import source from "vinyl-source-stream"
+import sass from "gulp-sass"
+import sassGlob from "gulp-sass-glob"
+import pleeease from "gulp-pleeease"
+import browserify from "browserify"
+import babelify from "babelify"
+import pug from "gulp-pug"
+import rename from "gulp-rename"
+import uglify from "gulp-uglify"
+import decodecode from "gulp-decodecode"
+import browserSync from "browser-sync"
+import readConfig from "read-config"
+import watch from "gulp-watch"
+import RevLogger from "rev-logger"
 
 // const
-const SRC = './src';
-const CONFIG = './src/config';
-const HTDOCS = '../../docs/2018';
-const BASE_PATH = '';
-const DEST = `${HTDOCS}${BASE_PATH}`;
+const SRC = "./src"
+const CONFIG = "./src/config"
+const HTDOCS = "../../docs/2018"
+const BASE_PATH = ""
+const DEST = `${HTDOCS}${BASE_PATH}`
 
 const revLogger = new RevLogger({
-    'style.css': `${DEST}/css/style.css`,
-    'script.deco.js': `${DEST}/js/script.deco.js`
-});
-
+  "style.css": `${DEST}/css/style.css`,
+  "script.deco.js": `${DEST}/js/script.deco.js`,
+})
 
 // css
-gulp.task('copy-bower-css', () => { 
-  return gulp.src(
-    [
-      'material-design-lite/material.min.css',
-      'material-design-lite/material.min.css.map'
-    ], {
-    cwd: 'node_modules',
-  })
+gulp.task("copy-bower-css", () => {
+  return gulp
+    .src(
+      [
+        "material-design-lite/material.min.css",
+        "material-design-lite/material.min.css.map",
+      ],
+      {
+        cwd: "node_modules",
+      }
+    )
     .pipe(gulp.dest(`${DEST}/css/lib`))
-  ;
-});
+})
 
-gulp.task('sass', () => {
-  const config = readConfig(`${CONFIG}/pleeease.json`);
-  return gulp.src(`${SRC}/scss/style.scss`)
+gulp.task("sass", () => {
+  const config = readConfig(`${CONFIG}/pleeease.json`)
+  return gulp
+    .src(`${SRC}/scss/style.scss`)
     .pipe(sassGlob())
     .pipe(sass())
     .pipe(pleeease(config))
     .pipe(gulp.dest(`${DEST}/css`))
-  ;
-});
+})
 
 // gulp.task('css', gulp.series(gulp.parallel('sass', 'copy-bower-css')));
-gulp.task('css', gulp.series('sass'));
-
+gulp.task("css", gulp.series("sass"))
 
 // js
-gulp.task('copy-bower-js', () => { 
-  return gulp.src(
-    [
-      // 'material-design-lite/material.min.js',
-      // 'material-design-lite/material.min.js.map',
-      'jquery/dist/jquery.min.js',
-      'jquery/dist/jquery.min.map',
-      'lodash/lodash.min.js'
-    ], {
-    cwd: 'node_modules',
-  })
+gulp.task("copy-bower-js", () => {
+  return gulp
+    .src(
+      [
+        // 'material-design-lite/material.min.js',
+        // 'material-design-lite/material.min.js.map',
+        "jquery/dist/jquery.min.js",
+        "jquery/dist/jquery.min.map",
+        "lodash/lodash.min.js",
+      ],
+      {
+        cwd: "node_modules",
+      }
+    )
     .pipe(gulp.dest(`${DEST}/js/lib`))
-  ;
-});
+})
 
-gulp.task('browserify', () => {
+gulp.task("browserify", () => {
   return browserify(`${SRC}/js/script.js`)
     .transform(babelify)
     .bundle()
-    .pipe(source('script.js'))
+    .pipe(source("script.js"))
     .pipe(gulp.dest(`${DEST}/js`))
-  ;
-});
+})
 
-gulp.task('minify', () => {
-  return gulp.src(`${DEST}/js/script.js`)
-    .pipe(uglify({
-      preserveComments: 'license',
-    }))
-    .pipe(rename('script.min.js'))
+gulp.task("minify", () => {
+  return gulp
+    .src(`${DEST}/js/script.js`)
+    .pipe(
+      uglify({
+        preserveComments: "license",
+      })
+    )
+    .pipe(rename("script.min.js"))
     .pipe(gulp.dest(`${DEST}/js`))
-  ;
-});
+})
 
-gulp.task('deco', () => {
-  return gulp.src(`${DEST}/js/script.js`)
-    .pipe(decodecode({
-      preserveComments: 'license',
-      decoArr: ['恵', '大', '毘', '弁', '布', '福', '寿'],
-    }))
-    .pipe(rename('script.deco.js'))
+gulp.task("deco", () => {
+  return gulp
+    .src(`${DEST}/js/script.js`)
+    .pipe(
+      decodecode({
+        preserveComments: "license",
+        decoArr: ["恵", "大", "毘", "弁", "布", "福", "寿"],
+      })
+    )
+    .pipe(rename("script.deco.js"))
     .pipe(gulp.dest(`${DEST}/js`))
-  ;
-});
+})
 
 // gulp.task 'js', gulp.parallel('browserify', 'copy-bower-js')
-gulp.task('js', gulp.series(gulp.parallel('browserify', 'copy-bower-js'), gulp.parallel('minify', 'deco')));
-
+gulp.task(
+  "js",
+  gulp.series(
+    gulp.parallel("browserify", "copy-bower-js"),
+    gulp.parallel("minify", "deco")
+  )
+)
 
 // html
-gulp.task('pug', () => {
+gulp.task("pug", () => {
   const locals = {
     meta: readConfig(`${CONFIG}/meta.yml`),
     versions: revLogger.versions(),
-  };
+  }
 
-  return gulp.src([`${SRC}/pug/**/[!_]*.pug`, `!${SRC}/pug/**/_*/**/*`])
-    .pipe(pug({
-      locals: locals,
-      pretty: true,
-      basedir: `${SRC}/pug`,
-    }))
+  return gulp
+    .src([`${SRC}/pug/**/[!_]*.pug`, `!${SRC}/pug/**/_*/**/*`])
+    .pipe(
+      pug({
+        locals: locals,
+        pretty: true,
+        basedir: `${SRC}/pug`,
+      })
+    )
     .pipe(gulp.dest(`${DEST}`))
-  ;
-});
+})
 
-gulp.task('html', gulp.series('pug'));
+gulp.task("html", gulp.series("pug"))
 
-
-gulp.task('browser-sync' , () => {
+gulp.task("browser-sync", () => {
   browserSync({
     server: {
-      baseDir: HTDOCS
+      baseDir: HTDOCS,
     },
     startPath: BASE_PATH,
     ghostMode: false,
-  });
+  })
 
-  watch([`${SRC}/scss/**/*.scss`], gulp.series('sass', browserSync.reload));
-  watch([`${SRC}/js/**/*.js`], gulp.series('js', browserSync.reload));
-  watch([
-      `${SRC}/pug/**/*.pug`,
-      `${SRC}/config/meta.yml`
-  ], gulp.series('pug', browserSync.reload));
+  watch([`${SRC}/scss/**/*.scss`], gulp.series("sass", browserSync.reload))
+  watch([`${SRC}/js/**/*.js`], gulp.series("js", browserSync.reload))
+  watch(
+    [`${SRC}/pug/**/*.pug`, `${SRC}/config/meta.yml`],
+    gulp.series("pug", browserSync.reload)
+  )
 
-  revLogger.watch((changed) => {
-      gulp.series('pug', browserSync.reload)();
-  });
-});
+  revLogger.watch(changed => {
+    gulp.series("pug", browserSync.reload)()
+  })
+})
 
-gulp.task('serve', gulp.series('browser-sync'));
+gulp.task("serve", gulp.series("browser-sync"))
 
-gulp.task('build', gulp.parallel('css', 'js', 'html'));
-gulp.task('default', gulp.series('build', 'serve'));
+gulp.task("build", gulp.parallel("css", "js", "html"))
+gulp.task("default", gulp.series("build", "serve"))
